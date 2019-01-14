c34d61a1fc973cedd2176dbd55699d863990e90b
Chelly Compendio <rochellecompendio2021@u.northwestern.edu>
2019-01-14 16:14:03 -0600

    Initial commit

---
+++ scroll.js
@@ -0,0 +1,21 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['landing', 'about', 'projects', 'design', 'contact'],
+    sectionsColor: ['#f1f1f1','white', '#f1f1f1', 'white', '#f1f1f1'],
+    css3: true,
+    scrollOverflow: true
+});
+
+$(document).ready(function(){
+    //fade page in on load------------------------------------------
+    $('body').css('display', 'none');
+    $('body').fadeIn(1000);
+});
+
+function myFunction() {
+    var x = document.getElementById("header");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
